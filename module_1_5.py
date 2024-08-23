886de07ce01bfe0fb0cd0e0bf956e2d766a86c88
Alexander-Logvin <shura.logvin@yandex.ru>
2024-08-23 17:10:53 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,8 +1,7 @@
-from dataclasses import replace
-
 immutable_var = 1, 2, 'ДЗ'
 print(immutable_var)
 #immutable_var[0] = 3 # Получаем ошибку 'tuple' object does not support item assignment означающую что нельзя менять данные в "Кортежи"
-mutable_var = '1, 2, ДЗ'
+mutable_var = [1, 2, 'ДЗ']
 print(type(mutable_var))
-print(mutable_var.replace('1', '3'))
+mutable_var[0] = 3
+print(mutable_var)
